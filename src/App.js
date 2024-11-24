aa7b653cdea69544beb72201c42c2b97518160ed
Luvkush Divakar <luvkushdivakar846@gmail.com>
2024-11-24 19:22:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [count, setcount] = useState(0);
+  const [type, settype] = useState();
+
+  const Addcounter = () => {
+    if (count < 10) {
+      setcount(count + 1);
+      settype(" Value is positive");
+    } 
+    else {
+      settype("You have Maximum limit");
+    }
+  }
+
+
+const SubtCounter = () => {
+  if (count) {
+    setcount(count - 1);
+    settype("Value is positive");
+  } 
+  else {
+    settype("you have minimum number limit ");
+  }
+}
+
+
+return (
+  <div className="App">
+    <h1>Type is:{type}</h1>
+    <h1>Count is : {count}</h1>
+
+    <button onClick={Addcounter}>+</button>
+    <button onClick={SubtCounter}>-</button>
+
+  </div>
+);
 }
 
 export default App;
